2eb967b9ccbe64945c8b0117606ce63afb231683
Allex Machado <itsnotallexmachado@gmail.com>
2025-06-05 13:00:34 -0300

    Update

---
+++ study009/segunda tentativa/script.js
@@ -0,0 +1,11 @@
+/*
+    Objetivo:
+        1 - Pegar o valor informado
+        2 - Pegar a categoria
+        3 - Impedir que o valor seja negativo
+        4 - O valor seja atualizado de acordo com a categoria
+            . Criar variáveis para controlar e armazenar os valores das categorias
+        5 - Atualizar os valores
+        6 - Limpar o campo de valor
+        7 - Exibir os valores atualizados na tela
+*/
